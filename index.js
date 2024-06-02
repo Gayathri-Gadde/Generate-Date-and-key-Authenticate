81e4edb973e4afe70209eb10cf7642190e6ed61b
Gadde Gayathri <t-ggayathri@microsoft.com>
2024-06-02 23:51:19 +0530

    after updating

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 const core = require('@actions/core');
 const axios = require('axios');
 
-async function getCurrentDateTime(functionUrl) {
-    try {
-        const response = await axios.get(functionUrl, {
-          headers: {
-            'x-functions-key': functionKey
-          }
-        });
-        return response.data;
-    } catch (error) {
-        throw new Error(`Failed to retrieve date and time from Azure Function: ${error.message}`);
-    }
-}
-
 async function run() {
-    try {
-        // Get the URL of the Azure Function endpoint from inputs
-        const functionUrl = core.getInput('function-url');
-        const functionKey = core.getInput('function-key');
-
-        // Call the Azure Function to get the current date and time
-        const currentDateTime = await getCurrentDateTime(functionUrl);
-
-        // Set the output with the current date and time
-        core.setOutput('currentDateTime', currentDateTime);
-    } catch (error) {
-        // Set failure message if an error occurs
-        core.setFailed(error.message);
-    }
+  try {
+    // Retrieve the inputs defined in action.yml
+    const functionUrl = core.getInput('function-url');
+    const functionkey = core.getInput('function-key');
+    
+    console.log(`Fetching data from URL: ${functionUrl}`);
+    
+    // Make the HTTP GET request to the Azure Function with the function key in the headers
+    const response = await axios.get(functionUrl, {
+      headers: {
+        'function-key': functionKey
+      }
+    });
+    
+    // Extract the data from the response
+    const currentDateTime = response.data;
+    console.log(`Response data: ${currentDateTime}`);
+    
+    // Set the output for the GitHub Action
+    core.setOutput('currentDateTime', currentDateTime);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-// Execute the action
 run();
